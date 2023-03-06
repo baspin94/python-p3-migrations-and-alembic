e23e7d8fd04e7e0fa3879fbf4dfeca10ffb46302
Bianca Aspin <bianca@mj94.net>
2023-03-05 22:29:10 -0800

    Complete codealong.

---
+++ lib/migrations/versions/440390244830_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 440390244830
+Revises: 
+Create Date: 2023-03-05 16:49:31.542206
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '440390244830'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
